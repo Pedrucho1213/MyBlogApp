cd388577cf8a878d7d1f3e385d2190ff49957c84
Pedro Miguel Pérez Torres <pmpedrotorres@gmail.com>
2023-10-13 20:18:00 -0600

    Firebase implementation

--- app/src/main/java/com/example/myblogapp/MainActivity.kt
+++ app/src/main/java/com/example/myblogapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myblogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
