5dd3b382cc52b3864cbe2be1fcde319f6f091e93
Pedro Miguel Pérez Torres <pmpedrotorres@gmail.com>
2023-10-16 11:23:51 -0600

    added form fragment validation

--- app/src/main/java/com/example/myblogapp/view/NewPostFragment.kt
+++ app/src/main/java/com/example/myblogapp/view/NewPostFragment.kt
@@ -1,6 +1,8 @@
 package com.example.myblogapp.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,10 +26,46 @@ class NewPostFragment : BottomSheetDialogFragment() {
         viewModel = ViewModelProvider(this)[PostViewModel::class.java]
     }
 
-    private fun inputValidations(){
-        binding.titleEvent.setOnEditorActionListener { v, actionId, event ->
-            return@setOnEditorActionListener actionId == EditorInfo.IME_ACTION_NEXT
-        }
+    private fun inputValidations() {
+        binding.titleEvent.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // TODO
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if ((s?.length ?: 0) > 15) {
+                    binding.titleEvent.error = "The title must be no longer than 15 characters"
+                    binding.saveBtn.isEnabled = false
+                } else {
+                    binding.titleEvent.error = null
+                    binding.saveBtn.isEnabled = true
+                }
+            }
+        })
+
+        binding.contentEvent.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // TODO
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if ((s?.length ?: 0) > 255) {
+                    binding.contentEvent.error = "The content must be no longer than 255 characters"
+                    binding.saveBtn.isEnabled = false
+                } else {
+                    binding.contentEvent.error = null
+                    binding.saveBtn.isEnabled = true
+                }
+            }
+        })
 
         binding.contentEvent.setOnEditorActionListener { v, actionId, event ->
             return@setOnEditorActionListener actionId == EditorInfo.IME_ACTION_DONE
@@ -37,6 +75,7 @@ class NewPostFragment : BottomSheetDialogFragment() {
             saveBtn()
         }
     }
+
     private fun saveBtn() {
         val title = binding.titleTxt.editText?.text.toString()
         val content = binding.contentTxt.editText?.text.toString()
